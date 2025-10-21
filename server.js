3e08976fdd21ac1e870cf977db2e3455c9ea7213
EXCALIBUR-cmd <singhmaninder93899@gmail.com>
2025-10-21 14:31:44 +0530

    updated server.js

--- server.js
+++ server.js
@@ -1,14 +1,9 @@
 require("dotenv").config();
-console.log("Environment variables loaded:");
-console.log("GEMINI_API_KEY exists:", !!process.env.GEMINI_API_KEY);
-console.log("MONGODB_URI:", process.env.MONGODB_URI);
-
 const app = require("./src/app");
 const connectDB = require("./src/db/db");
 
 const PORT = process.env.PORT || 3000;
 
-// Connect to MongoDB
 connectDB();
 
 app.listen(PORT, () => {
